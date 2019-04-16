9e8703bb97ab3ba0d6ab254c074bdda5cabab59e
Pruthvi Bhogadi <pbhogadi@altimetrik.com>
2019-04-16 14:57:48 +0530

    Added Chapter3-Exercise2

---
+++ test/clojureexercises/chapter3/exercise2.clj
@@ -0,0 +1,9 @@
+(ns clojureexercises.exercise2
+  (:require [clojure.test :refer :all]
+            [clojureexercises.core :refer :all]))
+
+(def number 100)
+(println "Enter the number : ")
+(let [n (Long/parseLong(read-line))] (def total (+ number n )))
+(println(str "After additiona, Total is " total))
+
